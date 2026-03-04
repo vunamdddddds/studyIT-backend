2b893c3ade0dd3123c66b977577f25bed249898c
vunamdddddds <dochet25@gmail.com>
2026-03-04 19:27:52 +0700

    tạo biến môi tường backend

--- src/main/java/com/cmcu/itstudy/ItstudyApplication.java
+++ src/main/java/com/cmcu/itstudy/ItstudyApplication.java
@@ -1,12 +1,13 @@
 package com.cmcu.itstudy;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class ItstudyApplication {
-
 	public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(ItstudyApplication.class, args);
 	}
 
